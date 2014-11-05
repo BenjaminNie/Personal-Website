38f0828fd97f80b99feacd1d645537f00def317f
Ben <benjamin.nie.ubc@gmail.com>
2014-11-05 00:52:15 -0600

    working version of blog.php with newly added automatic parsing of json files

--- helper_fxn.php
+++ helper_fxn.php
@@ -3,7 +3,7 @@
 function getFileName ($month, $year) 
 {
 	//convert month to string and get new json file name
-	return "{$currentYear}" . "-" . sprintf("%02s", $currentMonth) . ".json";
+	return "{$year}" . "-" . sprintf("%02s", $month) . ".json";
 }
 
 function getNewDate(&$month, &$year)
